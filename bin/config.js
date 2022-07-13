632f97d7a0991ed52d6792d5ea9e3510440752ca
hirayukoji <109202723+hirayukoji@users.noreply.github.com>
2022-07-13 15:56:51 +0900

    Initial commit

---
+++ bin/config.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path")
+const srcConfigFilePath = path.join(process.cwd(), "/src/config.json");
+
+let config;
+try {
+  config = JSON.parse(fs.readFileSync(srcConfigFilePath));
+
+  const envText =
+    Object.keys(config)
+      // オブジェクト等は環境変数として出力しない
+      .filter((key) => typeof config[key] === "string" || typeof config[key] === "number")
+      .map((key) => `REACT_APP_${key.toUpperCase()}="${config[key]}"`)
+      .join("\n");
+
+  fs.writeFileSync(path.join(process.cwd(), '.env'), envText)
+
+} catch (error) {
+  process.stderr.write(`${srcConfigFilePath} が存在しません。\n`);
+  process.exit(1);
+}
+
+
+
